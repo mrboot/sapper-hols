4a594a9f94b3077c235dcfc214092f2c17c1afb3
Mark Booth <mrbooth@gmail.com>
2020-07-26 14:39:23 +0100

    linting fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
-import css from 'rollup-plugin-css-only';
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import commonjs from '@rollup/plugin-commonjs'
+import svelte from 'rollup-plugin-svelte'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import config from 'sapper/config/rollup.js'
+import pkg from './package.json'
+import css from 'rollup-plugin-css-only'
 import sapperEnv from 'sapper-environment'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
 const apiBaseUrl = process.env.BASE_API_URL
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning)
 
 export default {
   client: {
@@ -37,24 +37,24 @@ export default {
       }),
       resolve({
         browser: true,
-        dedupe: ['svelte']
+        dedupe: [ 'svelte' ]
       }),
       commonjs(),
-      css({ output: "static/custom.css" }),
+      css({ output: 'static/custom.css' }),
 
       legacy && babel({
-        extensions: ['.js', '.mjs', '.html', '.svelte'],
+        extensions: [ '.js', '.mjs', '.html', '.svelte' ],
         runtimeHelpers: true,
-        exclude: ['node_modules/@babel/**'],
+        exclude: [ 'node_modules/@babel/**' ],
         presets: [
-          ['@babel/preset-env', {
+          [ '@babel/preset-env', {
             targets: '> 0.25%, not dead'
-          }]
+          } ]
         ],
         plugins: [
-          '@babel/plugin-syntax-dynamic-import', ['@babel/plugin-transform-runtime', {
+          '@babel/plugin-syntax-dynamic-import', [ '@babel/plugin-transform-runtime', {
             useESModules: true
-          }]
+          } ]
         ]
       }),
 
@@ -79,7 +79,7 @@ export default {
         dev
       }),
       resolve({
-        dedupe: ['svelte']
+        dedupe: [ 'svelte' ]
       }),
       commonjs()
     ],
@@ -104,4 +104,4 @@ export default {
 
     onwarn,
   }
-};
+}
